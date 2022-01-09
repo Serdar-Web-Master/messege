8256490be0db32e4b0a1bbd3202ce05397a5aa0a
Serdar-Web-Master <serdarcholukov@yandex.com>
2022-01-09 16:31:10 +0500

    09-01

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
 const m0801 = document.querySelector("#m0801");
 const m0804 = document.querySelector("#m0804");
 const popup__0801 = document.querySelector(".popup__0801");
+const popup__0804 = document.querySelector(".popup__0804");
+const back = document.querySelectorAll(".back");
+
 
 m0801.addEventListener("click", () => {
   popup__0801.style.display = "block";
+});
+
+m0804.addEventListener("click", () => {
+  popup__0804.style.display = "block";
+});
+
+back[0].addEventListener("click", () => {
+  popup__0801.style.display = "none";
+});
+back[1].addEventListener("click", () => {
+  popup__0804.style.display = "none";
 });
